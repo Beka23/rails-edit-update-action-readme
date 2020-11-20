90df6b825a9ef429d56baaf6ee866a8875e8f1b5
Beka23 <beksultankaiypov@gmail.com>
2020-11-20 13:54:58 -0500

    commit tree 80921b5f0f475cb5c359fb256500be7ebbbb5d40

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,7 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+
   def create
     @article = Article.new
     @article.title = params[:title]
@@ -20,4 +21,18 @@ class ArticlesController < ApplicationController
   end
 
   # add edit and update methods here
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+   
+  def update
+    @article = Article.find(params[:id])
+    @article.update(title: params[:article][:title], description: params[:article][:description])
+    redirect_to article_path(@article)
+  end 
+  
+
+
 end
